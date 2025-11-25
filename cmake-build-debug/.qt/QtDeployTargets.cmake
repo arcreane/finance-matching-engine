616567af46f79ac874d4e1462de901416f2352df
LOD <104134005+LOD314@users.noreply.github.com>
2025-11-25 15:08:53 +0100

    Delete cmake-build-debug directory
    patch

--- cmake-build-debug/.qt/QtDeployTargets.cmake
+++
@@ -1,2 +0,0 @@
-set(__QT_DEPLOY_TARGET_MatchingEngine_FILE /Users/elodie/Documents/GitHub/finance-matching-engine/cmake-build-debug/MatchingEngine)
-set(__QT_DEPLOY_TARGET_MatchingEngine_TYPE EXECUTABLE)
